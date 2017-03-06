21fe58dbac7a3626aedc7c868b172cf7101d49cc
Odinaris <1026250255@qq.com>
2017-03-06 15:38:13 +0800

    新建RentCar,BuyCar,SaleCar三个Fragment作为买卖租车的入口。

---
+++ app/src/main/java/me/odinaris/searchcar/sale_car/SaleCarFragment.kt
@@ -0,0 +1,23 @@
+package me.odinaris.searchcar.sale_car_car
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import butterknife.ButterKnife
+import me.odinaris.searchcar.R
+
+/**
+ * Created by Odinaris on 2017/3/5.
+ */
+
+class SaleCarFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view : View = inflater!!.inflate(R.layout.frag_homepage,container,false)
+        ButterKnife.bind(this,view)
+        return view
+    }
+}
