722916962ac1404d7a924ca4ec9c4b484dd970ab
Odinaris <1026250255@qq.com>
2017-03-13 00:24:25 +0800

    添加LoginActivity

---
+++ app/src/main/java/me/odinaris/searchcar/login/LoginActivity.kt
@@ -0,0 +1,11 @@
+package me.odinaris.searchcar.login
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Odinaris on 2017/3/13.
+ */
+
+class LoginActivity : AppCompatActivity() {
+
+}
