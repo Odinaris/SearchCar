b9fdbd8be9a2eaba28334d02e7a161684fb5c990
Odinaris <1026250255@qq.com>
2017-03-17 00:01:54 +0800

    修改submit Button为满足输入条件才可点击

--- app/src/main/java/me/odinaris/searchcar/login/LoginActivity.kt
+++ app/src/main/java/me/odinaris/searchcar/login/LoginActivity.kt
@@ -2,6 +2,8 @@ package me.odinaris.searchcar.login
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.MenuItem
 import cn.bmob.v3.BmobUser
 import me.odinaris.searchcar.R
@@ -17,12 +19,27 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //设置标题栏返回键
         val actionBar = supportActionBar
         if (actionBar != null) {
             actionBar.setHomeButtonEnabled(true)
             actionBar.setDisplayHomeAsUpEnabled(true)
         }
         setContentView(R.layout.act_login)
+        btn_submit.isClickable = false
+        et_password.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(var1: CharSequence, var2: Int, var3: Int, var4: Int) {
+
+            }
+
+            override fun onTextChanged(var1: CharSequence, var2: Int, var3: Int, var4: Int) {
+                btn_submit.isClickable = true
+            }
+
+            override fun afterTextChanged(var1: Editable) {
+
+            }
+        })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
