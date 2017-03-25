3bef1c7dcef6f4c8567a405ea7ff614a4801ff96
Odinaris <1026250255@qq.com>
2017-03-26 02:53:53 +0800

    添加Fragment间跳转代码，但有bug待解决，另引入第三方RecyclerView库实现上拉加载下拉刷新功能

--- app/src/main/java/me/odinaris/searchcar/homepage/HomepageFragment.kt
+++ app/src/main/java/me/odinaris/searchcar/homepage/HomepageFragment.kt
@@ -7,6 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import me.odinaris.searchcar.R
+import kotlinx.android.synthetic.main.frag_homepage.*
+import me.odinaris.searchcar.buy_car.BuyCarFragment
+import me.odinaris.searchcar.rent_car.RentCarFragment
+import me.odinaris.searchcar.sale_car_car.SaleCarFragment
+
 
 /**
  * Created by Odinaris on 2017/3/5.
@@ -17,5 +22,37 @@ class HomepageFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view : View = inflater!!.inflate(R.layout.frag_homepage,container,false)
         return view
+    }
+
+    override fun onViewCreated(view: View,savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initClickListener()
+    }
+
+    private fun initClickListener() {
+        bbl_car_buy.setOnClickListener({
+            fragmentManager!!.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.main_container, BuyCarFragment())
+                    .commit()
+        })
+        bbl_car_rent.setOnClickListener({
+            fragmentManager!!.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.main_container, RentCarFragment())
+                    .commit()
+        })
+        bbl_car_sale.setOnClickListener({
+            fragmentManager!!.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.main_container, SaleCarFragment())
+                    .commit()
+        })
+
+    }
+
+    fun initView(){
+
     }
 }
